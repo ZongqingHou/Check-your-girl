ef350d420812f6424c836d3d99f6d58ec4d48ce8
hdD <zongqingh@gmail.com>
2017-12-10 16:17:41 +0800

    interface

--- show_image.py
+++ show_image.py
@@ -1,41 +1,76 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.uix.image import Image
+from kivy.uix.boxlayout import BoxLayout
 
-class Image(Widget):
-	def __init__():
-		pass
+import glob
 
-class ShowImage(App):
-	def build(self):
-		parent = Image()
+LABEL_LIST = []
+IMAGE_COUNT = 0
+BASE_FILE_PATH = '../data/*.jpg'
 
-		btn_good = Button(text='good')
-		btn_notgood = Button(text='not good',pos=(parent.width,0))
-		btn_sexy = Button(text='sexy',pos=(2*parent.width,0))
-		btn_notclear = Button(text='not clear',pos=(3*parent.width,0))
+PATH = glob.iglob(BASE_FILE_PATH)
 
-		btn_good.bind(on_release=self.set_good)
-		btn_notgood.bind(on_release=self.set_notgood)
-		btn_sexy.bind(on_release=self.set_sexy)
-		btn_notclear.bind(on_release=self.set_notclear)
+class HelloApp(App):
+    def build(self):
+    	return ShowImageLayout()
 
-		parent.add_widget(self.painter)
-		parent.add_widget(btn_good)
-		parent.add_widget(btn_notgood)
-		parent.add_widget(btn_sexy)
-		parent.add_widget(btn_notclear)
-		return parent
+class ShowImageLayout(BoxLayout):
+	def __init__(self, **kwargs):
+		super(ShowImageLayout, self).__init__(**kwargs)
+		self.source = next(PATH)
 
-	def set_good(self, obj):
-		pass
+		good_btn = GoodBtn()
+		notgood_btn = NotGoodBtn()
+		sexy_btn = SexyBtn()
+		notclear_btn = NotClearBtn()
 
-	def set_notgood(self, obj):
-		pass
+		good_btn.bind(on_release=self.set_source)
+		notgood_btn.bind(on_release=self.set_source)
+		sexy_btn.bind(on_release=self.set_source)
+		notclear_btn.bind(on_release=self.set_source)
 
-	def set_sexy(self, obj):
-		pass
+		self.add_widget(good_btn)
+		self.add_widget(notgood_btn)
+		self.add_widget(sexy_btn)
+		self.add_widget(notclear_btn)
 
-	def set_notclear(self, obj):
-		pas
+	def set_source(self, obj):
+		global PATH
+		self.source = next(PATH)
+
+class GoodBtn(Button):
+	def onclick(self):
+		global LABEL_LIST
+		global IMAGE_COUNT
+
+		LABEL_LIST.append(0)
+		IMAGE_COUNT += 1
+
+class NotGoodBtn(Button):
+	def onclick(self):
+		global LABEL_LIST
+		global IMAGE_COUNT
+
+		LABEL_LIST.append(1)
+		IMAGE_COUNT += 1
+
+class SexyBtn(Button):
+	def onclick(self):
+		global LABEL_LIST
+		global IMAGE_COUNT
+
+		LABEL_LIST.append(2)
+		IMAGE_COUNT += 1
+
+class NotClearBtn(Button):
+	def onclick(self):
+		global LABEL_LIST
+		global IMAGE_COUNT
+
+		LABEL_LIST.append(-1)
+		IMAGE_COUNT += 1
+
+if __name__ == '__main__':
+	HelloApp().run()
